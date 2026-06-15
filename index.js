cadf62b7b64386763da6cb4faba0ede9ca706454
Mikal-E <melamin101@gmail.com>
2026-06-15 14:57:12 -0400

    Initial Repository

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 3000
+
+app.set("view engine", "ejs");
+
+app.listen(port, () => {
+
+  console.log(`Server is running on port ${port}`);
+
+});
