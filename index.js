2958b9e907c51cc8b6d1b30f53b3eb23b40fb194
Mikal-E <melamin101@gmail.com>
2026-06-16 20:45:03 -0400

    Finished the last parts of the lab, that were not completed in class.

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ import express from "express";
 
 import morgan from "morgan";
 
+import path from "path";
+
 const app = express();
 const port = 3000
 
-// Setting view engine
-
 app.set("view engine", "ejs");
 
-// Send a simple "success" response
+// Part 3 - Use Express Static Middleware To Open Up The Assets Folder For Image
+
+app.use(express.static("assets"));
+
+// Part 2 - app.use for Morgan Middleware 
+
+app.use(morgan("dev"));
+
+// Routes - Send a simple "success" response
 
 app.post("/submit", (req, res) => {
 
@@ -21,18 +29,39 @@ app.post("/submit", (req, res) => {
 
 })
 
-// Part 2 - app.use for Morgan Middleware 
-
-app.use(morgan("dev"));
-
 // Create at least two different view templates for your chosen view engine
 
 app.get('/', (req, res) => { res.render('home', { title: 'App', message: 'Welcome!' }); });
 app.get('/about', (req, res) => { res.render('about', { title: 'App', message: 'Welcome!' }); });
 
+// Image Download Route.
+
+// const path = require("path");
+
+app.get("/download-image", (req, res) => {
+
+  const knicksImage = path.join(import.meta.dirname, "assets", "images", "2026-nba-champions-knicks.png");
+
+  res.download(knicksImage, "2026-nba-champions-knicks.png", (err) => {
+
+    if (err) {
+
+      console.error("Download failed:", err);
+
+      if (!res.headersSent) {
+
+        res.status(500).send("Could not download the file.")
+        
+      }
+    }
+  })
+})
+
 // Within your routes, include at least one instance of a route parameter that modifies what is rendered to the client.
 
 app.get("/:name", (req, res) => { res.render("about") });
+
+
 
 // Tell server which port to listen on
 app.listen(port, () => {
@@ -40,4 +69,6 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 
 });
+
+
 
